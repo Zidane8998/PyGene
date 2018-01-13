4ff59922f66a788df1f31ba225580a5568360421
Andrew Hansen <subtonesrock@gmail.com>
2018-01-13 13:39:52 -0800

    Added game files to git.

---
+++ test.py
@@ -0,0 +1,42 @@
+import pygame, sys, random
+from pygame.locals import *
+
+pygame.init()
+
+size = width, height = 800, 600
+speed = [1, 1]
+black = 0, 0, 0
+
+screen = pygame.display.set_mode(size)
+
+world = []
+
+x = random.randrange(0, width)
+y = random.randrange(0, height)
+
+loc = [x, y]
+
+ball = pygame.image.load("../assets/ball.gif")
+ballrect = ball.get_rect()
+ballrect = ballrect.move(loc)
+world.append(ballrect)
+
+while 1:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+
+    screen.fill(black)
+    for ballrect in world:
+        ballrect = world.pop()
+        ballrect = ballrect.move(speed)
+        if ballrect.left < 0 or ballrect.right > width:
+            speed[0] = -speed[0]
+        if ballrect.top < 0 or ballrect.bottom > height:
+            speed[1] = -speed[1]
+        world.append(ballrect)
+        screen.blit(ball, ballrect)
+
+    pygame.display.flip()
+
+
